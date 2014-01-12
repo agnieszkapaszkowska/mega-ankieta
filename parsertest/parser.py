a124136d09abdc4e87065b91fee0a44e7e2f4cfb
rudimichal <rudimichal@gmail.com>
2014-01-12 15:37:11 +0100

    Grammar 5.0

--- parsertest/parser.py
+++ parsertest/parser.py
@@ -12,12 +12,12 @@ def print_tree(tree, level):
 		pass
 	else:
 		for el in tree:
-			print(level * "\t" + "{0} ({1}-{2} \"{3}\")".format(el[0],el[1],el[2], input[el[1]:el[2]]))
+			print(level * "\t" + "{0} ({1}-{2} \"{3}\")".format(el[0],el[1],el[2], re.sub(r'\n', r'\\n', input[el[1]:el[2]])))
 			print_tree(el[3], level + 1)
 
 
-decl = open('condition.def').read()
-parser = generator.buildParser(decl).parserbyname('condition')
+decl = open('grammar.def').read()
+parser = generator.buildParser(decl).parserbyname('survey')
 success, resultTrees, nextCharacter = TextTools.tag(input, parser)
 
 print_tree(resultTrees, 0)
