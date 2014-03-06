421b390aa372cb9220bacc8d6851697ea2d32561
Hubert Marlikowski <hm306317@students.mimuw.edu.pl>
2014-03-06 09:22:26 +0100

    Grammar tests 2.0

--- parsertest/parsertester.py
+++ parsertest/parsertester.py
@@ -9,13 +9,26 @@ parser = Parser(decl)
 
 for i in range(0, len(testCases)):
 	input, production, expectedResult = testCases[i]
-	success, children, nextCharacter = parser.parse(input, production=production)
+	success, children, nextCharacter = parser.parse(input,
+			production=production)
 	try:
-		assert success == expectedResult and nextCharacter==len(input)
+		assert success == expectedResult and ((expectedResult == 1 and nextCharacter==len(input)) or expectedResult == 0)
 		print(".", end=""),
 	except AssertionError:
 		if len(argv) > 1 and (argv[1] == '-v' or argv[1] == '--verbose'):
-			print("""\nTest %s error:\nWasn't able to parse %s as a %s (%s chars parsed of %s), returned value was %s""" % ( i, input, production, nextCharacter, len(input), (success, children, nextCharacter)))
-			break
+			if expectedResult == 1:
+				print("\nTest %s error:\nWasn't able to parse %s as a %s (%s chars parsed of %s),"
+				"returned value was %s""" % 
+				( i, input, production, nextCharacter, len(input), 
+					(success, children, nextCharacter)))
+				break
+			else:
+				print("\nTest %s error:\nShouldn't have parsed %s as a %s (%s chars parsed of %s),"
+				"returned value was %s""" % 
+				( i, input, production, nextCharacter, len(input), 
+					(success, children, nextCharacter)))
+				break
+
+
 		else:
 			print('x', end='')
