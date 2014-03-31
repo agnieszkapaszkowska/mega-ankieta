9a7e4f63062cf08cf06ac88e38cb79efb4ce0cfc
ap321142 <ap321142@students.mimuw.edu.pl>
2014-03-31 14:40:39 +0200

    js text fix

--- django_app/iss/iss/static/surveys/qunit/widgetsTests.js
+++ django_app/iss/iss/static/surveys/qunit/widgetsTests.js
@@ -1,5 +1,5 @@
 test( "text_test1", function() {
-	var txt = new iss.lib.widgets.TextWidget({text: function(){ return "abc"}});
+	var txt = new iss.lib.widgets.Widget("text", {text: function(){ return "abc"}});
     var widget = txt(function() { return true })('#qunit-fixture', null);
     //console.log(widget.element[0].childNodes[0].data);
 	equal( widget.element[0].childNodes[0].data, "abc", "Pased!" );
